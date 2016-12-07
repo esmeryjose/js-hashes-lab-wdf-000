48ecc6f614d6e1fbe61de674147ec9e9a1fa6b5d
github username <github email address>
2016-12-07 13:35:17 -0500

    Done

--- recipe.js
+++ recipe.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function addIngredient(recipe,ingridientName,amount){
+  recipe[ingridientName]  = amount;
+  return recipe;
+}
+
+function removeIngredient(recipe, ingridientName){
+  delete recipe[ingridientName];
+  return recipe;
+}
+
+function updateIngredient(recipe,ingridientName,amount){
+  recipe[ingridientName]  = amount;
+  return recipe;
+}
+
+function readRecipe(recipe){
+  for (var property in recipe) {
+    if (recipe.hasOwnProperty(property)) {
+      console.log(`this recipe calls for ${recipe[property]} of ${property}`);
+    }
+  }
+}
